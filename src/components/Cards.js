1e3914e7d40696a809a96a2a6b10abb0f67a1fff
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-01 15:37:58 +0900

    deleted unnecessary card Items and text & label

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,42 +5,14 @@ import "./Cards.css";
 function Cards() {
 	return (
 		<div className="cards">
-			<h1>Check out these Epic Destinations!</h1>
+			{/* <h1>Check out these Epic Destinations!</h1> */}
 			<div className="container">
 				<div className="cards__wrapper">
 					<ul className="cards__items">
-						<CardItem
-							src="images/img-9.jpg"
-							text="Explore the hidden waterfall deep inside the Amazon Jungle"
-							label="Adventure"
-							path="/products"
-						/>
-						<CardItem
-							src="images/img-2.jpg"
-							text="Travel through the Islands of Bali in a Private Cruise"
-							label="Luxury"
-							path="/services"
-						/>
+						<CardItem src="images/12345.jpeg" path="/products" />
 					</ul>
 					<ul className="cards__items">
-						<CardItem
-							src="images/1234.jpeg"
-							text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-							label="Mystery"
-							path="/services"
-						/>
-						<CardItem
-							src="images/img-4.jpg"
-							text="Experience Football on Top of the Himilayan Mountains"
-							label="Adventure"
-							path="/products"
-						/>
-						<CardItem
-							src="images/img-8.jpg"
-							text="Ride through the Sahara Desert on a guided camel tour"
-							label="Adrenaline"
-							path="/signup"
-						/>
+						<CardItem src="images/1234.jpeg" path="/services" />
 					</ul>
 				</div>
 			</div>
