11bed4d3fd3b2d9f300a978431d05442715f6f3e
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-01 14:18:33 +0900

    put some texts on images

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -5,11 +5,11 @@ function CardItem(props) {
 	return (
 		<li className="cards__item">
 			<Link className="cards__item__link" to={props.path}>
-				<figure className="cards__item__pic-wrap" data-category={props.label}>
+				<div className="cards__item__pic-wrap" data-category={props.label}>
 					<img className="cards__item__img" src={props.src} alt="Travel"></img>
-				</figure>
-				<div className="cards__item__info">
-					<h5 className="cards__item__text">{props.text}</h5>
+					<div class="text-on-image">
+						<p> Welcome to my Blog </p>
+					</div>
 				</div>
 			</Link>
 		</li>
