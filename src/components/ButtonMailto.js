3f0ff87b463b8f470df77c383467f0f2b4d06e90
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-03 17:27:04 +0900

    made button comp and mailing feature

--- src/components/ButtonMailto.js
+++ src/components/ButtonMailto.js
@@ -1,27 +1,41 @@
 import React from "react";
-import "./Button.css";
 import { Link } from "react-router-dom";
+import "./Button.css";
 
 const STYLES = ["btn--primary", "btn--outline"];
 const SIZES = ["btn--medium", "btn--large"];
 
-const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
+const ButtonMailto = ({
+	mailto,
+	label,
+	type,
+	onClick,
+	buttonStyle,
+	buttonSize,
+}) => {
 	const checkButtonStyle = STYLES.includes(buttonStyle)
 		? buttonStyle
 		: STYLES[0];
 
 	const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
 	return (
-		<Link to="/footer" className="btn-mobile">
+		<Link
+			to="#"
+			onClick={(e) => {
+				window.location = mailto;
+				e.preventDefault();
+			}}
+			className="btn-mobile"
+		>
 			<button
 				className={`btn ${checkButtonStyle} ${checkButtonSize}`}
 				onClick={onClick}
 				type={type}
 			>
-				{children}
+				{label}
 			</button>
 		</Link>
 	);
 };
-export default Button;
+
+export default ButtonMailto;
