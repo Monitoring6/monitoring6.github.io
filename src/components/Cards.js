56627b6d6dbc39eead0cf0c253ce1ffdaf4a2979
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-03 17:25:14 +0900

    added some card components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,14 +5,13 @@ import "./Cards.css";
 function Cards() {
 	return (
 		<div className="cards">
-			{/* <h1>Check out these Epic Destinations!</h1> */}
 			<div className="container">
 				<div className="cards__wrapper">
+					{/* <ul className="cards__items">
+						<CardItem src="images/12345.jpeg" path="/vision" />
+					</ul> */}
 					<ul className="cards__items">
-						<CardItem src="images/12345.jpeg" path="/products" />
-					</ul>
-					<ul className="cards__items">
-						<CardItem src="images/1234.jpeg" path="/services" />
+						<CardItem src="images/mainpage.png" path="/culture" />
 					</ul>
 				</div>
 			</div>
