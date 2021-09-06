5c5f3ac4ab1a81d6756d96b3aa5f1a529a87ee1c
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-06 16:41:15 +0900

    About page design component

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../App.css";
-import "../Pages.css";
+import "./About.css";
+import Footer from "../Footer";
 
 export default function About() {
 	return (
@@ -11,24 +12,38 @@ export default function About() {
 			<div>
 				<div className="text-on-image">
 					<div className="text-on-image-1">
-						<h2>기술이 자라는 회사</h2>
+						<h2>
+							기술이 자라는 회사
+							<p className="p1">
+								기술이 자라요 <br />
+								<span className="space">
+									# 그 누구보다 빠르게 남들과는 다르게
+								</span>
+							</p>
+						</h2>
 
-						<h2>직원이 자라는 회사</h2>
+						<h2>
+							직원이 자라는 회사
+							<p className="p2">직원이 자라요</p>
+						</h2>
 					</div>
-					{/* <div className="text-on-image-2">
-						<h2>직원이 자라는 회사</h2>
-					</div> */}
 					<div className="text-on-image-3">
-						<h2>안전이 자라는 회사</h2>
+						<h2>
+							안전이 자라는 회사
+							<p className="p3">안전이 자라요</p>
+						</h2>
 
-						<h2>자율성이 자라는 회사</h2>
+						<h2>
+							자율성이 자라는 회사
+							<p className="p4">
+								자율성이 자라요 <br /> #자유와 방종 사이
+							</p>
+						</h2>
 					</div>
-					{/* <div className="text-on-image-4">
-						<h2>자율성이 자라는 회사</h2>
-					</div> */}
 				</div>
 				<div className="vl"></div>
 			</div>
+			<Footer></Footer>
 		</div>
 	);
 }
