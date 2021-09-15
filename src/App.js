669c8a31b0e8f96fb2b124e701a3536b01510fe3
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-15 17:00:46 +0900

    deleted unused codes

--- src/App.js
+++ src/App.js
@@ -5,25 +5,11 @@ import React from "react";
 import About from "./components/pages/About";
 import Culture from "./components/pages/Culture";
 import Vision from "./components/pages/Vision";
-import { Trans, useTranslation } from "react-i18next";
 import Home from "./components/pages/Home";
 
 function App() {
-	const { t, i18n } = useTranslation();
-
-	const changeLanguage = (language) => {
-		i18n.changeLanguage(language);
-	};
-
 	return (
 		<div className="App">
-			{/* <button onClick={() => changeLanguage("en")}>En</button>
-			<button onClick={() => changeLanguage("kr")}>Kr</button>
-			<hr />
-			<Trans i18nKey="description.part1">
-				To get started, edit <code>src/App.js</code> and save to reload.
-			</Trans>
-			<div>{t("description.part2")}</div> */}
 			<Router>
 				<Navbar></Navbar>
 				<Switch>
