c62a7170964805a7ef0ec83fb99eacaaf889c2ff
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-15 16:55:05 +0900

    added footer to culture page

--- src/components/pages/Culture.js
+++ src/components/pages/Culture.js
@@ -1,7 +1,13 @@
 import React from "react";
 // import "../../App.css";
 import Member from "../Member";
+import Footer from "../Footer";
 
 export default function Culture() {
-	return <Member></Member>;
+	return (
+		<div>
+			<Member></Member>
+			<Footer></Footer>
+		</div>
+	);
 }
