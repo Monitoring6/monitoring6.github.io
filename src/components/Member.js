6078d89ce18e41892fdf09d63372ff4947fc2751
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-17 15:28:22 +0900

    changed class name

--- src/components/Member.js
+++ src/components/Member.js
@@ -10,19 +10,19 @@ const Member = () => {
 					<div className="face-container1">
 						<div className="face-wrap">
 							<img className="face" src="images/W.h2.JPG" alt="William" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">정원혁(William)</div>
 							</div>
 						</div>
 						<div className="face-wrap">
 							<img className="face" src="images/Juy2.JPG" alt="Juy" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">이주영(Juy)</div>
 							</div>
 						</div>
 						<div className="face-wrap">
 							<img className="face" src="images/K.y.JPG" alt="KY" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">이강연(KY)</div>
 							</div>
 						</div>
@@ -30,19 +30,19 @@ const Member = () => {
 					<div className="face-container2">
 						<div className="face-wrap">
 							<img className="face" src="images/S.j.JPG" alt="Sanju" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">Sanju(김산쥬)</div>
 							</div>
 						</div>
 						<div className="face-wrap">
 							<img className="face" src="images/G.h.JPG" alt="Kyeonghyeon" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">조경현</div>
 							</div>
 						</div>
 						<div className="face-wrap">
 							<img className="face" src="images/J.H.JPG" alt="Junhyeok" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">권준혁</div>
 							</div>
 						</div>
@@ -50,20 +50,28 @@ const Member = () => {
 					<div className="face-container3">
 						<div className="face-wrap">
 							<img className="face" src="images/J.s.JPG" alt="Jongsub" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">최종섭</div>
 							</div>
 						</div>
 						<div className="face-wrap">
 							<img className="face" src="images/Y.m.JPG" alt="Youngmin" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">박영민</div>
 							</div>
 						</div>
 						<div className="face-wrap">
 							<img className="face" src="images/M.h.JPG" alt="Minho" />
-							<div className="william-hover">
+							<div className="image-hover">
 								<div className="text-on-hover">강민호</div>
+							</div>
+						</div>
+					</div>
+					<div className="face-container4">
+						<div className="face-wrap">
+							<img className="face" src="images/O.y.JPG" alt="eunyung" />
+							<div className="image-hover-1">
+								<div className="text-on-hover">송은영</div>
 							</div>
 						</div>
 					</div>
