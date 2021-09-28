d337fe32c245735cb96de4d8d55df8b6b0fe7a53
kangyeon0728 <kangyeon0728@gmail.com>
2021-09-28 18:38:25 +0900

    background video => img

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,9 +5,14 @@ import "../App.css";
 function HeroSection() {
 	return (
 		<div className="hero-container">
-			<video autoPlay muted loop playsInline>
+			{/* <video autoPlay muted loop playsInline>
 				<source src="/videos/mainpage-video.mp4" type="video/mp4"></source>
-			</video>
+			</video> */}
+			<img
+				src="/images/heroSection.jpg"
+				alt="heroSection"
+				className="background-img"
+			/>
 			<h1>BEYOND SAFETY</h1>
 			<p>
 				시공간의 제약 없이 <br /> 끊임없는 모니터링
